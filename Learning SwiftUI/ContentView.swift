94bbb90c5812aed376c0631937632690275d54ce
scorcherfjk <franjavikale@gmail.com>
2021-01-04 16:20:30 -0500

    Initial Commit

---
+++ Learning SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Learning SwiftUI
+//
+//  Created by Francisco De Freitas on 4/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Learning SwiftUI/Learning_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_SwiftUIApp.swift
+//  Learning SwiftUI
+//
+//  Created by Francisco De Freitas on 4/01/21.
+//
+
+import SwiftUI
+
+@main
+struct Learning_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
